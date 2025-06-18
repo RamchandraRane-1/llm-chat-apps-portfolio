ce38092fed3ff8473b9740e11d3dc44a3c19f08a
Ramchandra Rane <ramchandrarane57@gmail.com>
2025-06-18 17:18:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,57 +1,84 @@
-
 import streamlit as st
+from streamlit_chat import message
+# from langchain.llms import OpenAI   #This import has been replaced by the below import please
 from langchain_openai import ChatOpenAI
-import os
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+memory = ConversationBufferMemory(return_messages=True)
+from langchain.chains.conversation.memory import (ConversationBufferMemory, 
+                                                  ConversationSummaryMemory, 
+                                                  ConversationBufferWindowMemory
+               
+                                                  )
 
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-api_key = os.getenv("OPENAI_API_KEY")
-if not api_key:
-    st.error("❌ OPENAI_API_KEY environment variable not found.")
-    st.stop()
+if 'conversation' not in st.session_state:
+    st.session_state['conversation'] =None
+if 'messages' not in st.session_state:
+    st.session_state['messages'] =[]
+if 'API_Key' not in st.session_state:
+    st.session_state['API_Key'] =''
 
-# From here down is all the StreamLit UI
-st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
-st.header("Hey, I'm your Chat GPT")
-
-
-
-if "sessionMessages" not in st.session_state:
-     st.session_state.sessionMessages = [
-        SystemMessage(content="You are a helpful assistant.")
-    ]
+# Setting page title and header
+st.set_page_config(page_title="Chat GPT Clone")
+st.markdown("<h1 style='text-align: center;'>How can I assist you? </h1>", unsafe_allow_html=True)
 
 
-
-def load_answer(question):
-
-    st.session_state.sessionMessages.append(HumanMessage(content=question))
-
-    assistant_answer  = chat.invoke(st.session_state.sessionMessages)
-
-    st.session_state.sessionMessages.append(AIMessage(content=assistant_answer.content))
-
-    return assistant_answer.content
+st.sidebar.title("")
+st.session_state['API_Key']= st.sidebar.text_input("What's your API key?",type="password")
+summarise_button = st.sidebar.button("Summarise the conversation", key="summarise")
+if summarise_button:
+    summarise_placeholder = st.sidebar.write("Nice chatting with you my friend :\n\n"+st.session_state['conversation'].memory.buffer)
+    #summarise_placeholder.write("Nice chatting with you my friend :\n\n"+st.session_state['conversation'].memory.buffer)
 
 
-def get_text():
-    input_text = st.text_input("You: ")
-    return input_text
 
-chat = ChatOpenAI(
-    temperature=0,
-    openai_api_key=api_key  
-)
+def getresponse(userInput, api_key):
 
-user_input=get_text()
-submit = st.button('Generate')  
+    if st.session_state['conversation'] is None:
 
-if submit:
+        llm = ChatOpenAI(
+            temperature=0,
+            openai_api_key=api_key,
+            model_name='gpt-3.5-turbo' 
+        )
+
+        st.session_state['conversation'] = ConversationChain(
+            llm=llm,
+            verbose=True,
+            memory=ConversationSummaryMemory(llm=llm)
+        )
+
+    response=st.session_state['conversation'].predict(input=userInput)
+    print(st.session_state['conversation'].memory.buffer)
     
-    response = load_answer(user_input)
-    st.subheader("Answer:")
-    st.write(response)
 
+    return response
+
+
+
+response_container = st.container()
+# Here we will have a container for user input text box
+container = st.container()
+
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("Your question goes here:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
+
+        if submit_button:
+            st.session_state['messages'].append(user_input)
+            model_response=getresponse(user_input,st.session_state['API_Key'])
+            st.session_state['messages'].append(model_response)
+            
+
+            with response_container:
+                for i in range(len(st.session_state['messages'])):
+                        if (i % 2) == 0:
+                            message(st.session_state['messages'][i], is_user=True, key=str(i) + '_user')
+                        else:
+                            message(st.session_state['messages'][i], key=str(i) + '_AI')
+        
+             
+
+        
